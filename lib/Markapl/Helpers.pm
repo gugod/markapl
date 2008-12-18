ab645dd6d5b7c6f31a59fe8766a1ba94670ce8eb
Liu Kang-min <gugod@gugod.org>
2008-12-18 10:15:57 +0800

    Document for helpers

--- lib/Markapl/Helpers.pm
+++ lib/Markapl/Helpers.pm
@@ -5,6 +5,12 @@ use Markapl;
 use Exporter::Lite;
 our @EXPORT = qw(THs TDs ths tds heads cells);
 
+sub linklist {
+    my @links = @_;
+    ul {
+    }
+}
+
 sub heads {
     for (@_) { th { $_ } }
 }
@@ -23,6 +29,77 @@ sub cells {
     *tds = \&cells; 
 }
 
-
-
 1;
+
+=head1 NAME
+
+Markapl::Helpers - Practical helper methods
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 FUNCTIONS
+
+=over 4
+
+=item cells($item1, $item2, ...)
+
+Wrap C<< <td></td> >> tag around for each item in the argument list.
+Useful when showing tabular data. For example:
+
+    table {
+        while(my @data = $dbh->fetchrow_array) {
+          cells(@data);
+        }
+    }
+
+=item heads($item1, $item2, ...)
+
+Similar to C<cells>, but output seris of C<< <th></th> >> tag instead.
+For example:
+
+    table {
+        heads( qw(Name Age Gender Height Weight) );
+        while(my @data = $dbh->fetchrow_array) {
+          cells(@data);
+        }
+    }
+
+
+=back
+
+=head1 AUTHOR
+
+Kang-min Liu  C<< <gugod@gugod.org> >>
+
+=head1 LICENCE AND COPYRIGHT
+
+Copyright (c) 2008, Kang-min Liu C<< <gugod@gugod.org> >>.
+
+This is free software, licensed under:
+
+    The MIT (X11) License
+
+=head1 DISCLAIMER OF WARRANTY
+
+BECAUSE THIS SOFTWARE IS LICENSED FREE OF CHARGE, THERE IS NO WARRANTY
+FOR THE SOFTWARE, TO THE EXTENT PERMITTED BY APPLICABLE LAW. EXCEPT WHEN
+OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES
+PROVIDE THE SOFTWARE "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER
+EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE
+ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE SOFTWARE IS WITH
+YOU. SHOULD THE SOFTWARE PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL
+NECESSARY SERVICING, REPAIR, OR CORRECTION.
+
+IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING
+WILL ANY COPYRIGHT HOLDER, OR ANY OTHER PARTY WHO MAY MODIFY AND/OR
+REDISTRIBUTE THE SOFTWARE AS PERMITTED BY THE ABOVE LICENCE, BE
+LIABLE TO YOU FOR DAMAGES, INCLUDING ANY GENERAL, SPECIAL, INCIDENTAL,
+OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR INABILITY TO USE
+THE SOFTWARE (INCLUDING BUT NOT LIMITED TO LOSS OF DATA OR DATA BEING
+RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU OR THIRD PARTIES OR A
+FAILURE OF THE SOFTWARE TO OPERATE WITH ANY OTHER SOFTWARE), EVEN IF
+SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF
+SUCH DAMAGES.
